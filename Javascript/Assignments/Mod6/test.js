68d1eddc8e0259ade997633d276ae913eb5b5d7a
SamerSiam <ssiam05@gmail.com>
2021-11-10 09:20:32 +0200

    a

---
+++ Javascript/Assignments/Mod6/test.js
@@ -0,0 +1,10 @@
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    for (let i=0; i< l.length; i++)
+      {
+        if (typeof l[i] ==='string' )
+          {
+            l[i].drop;
+          }
+      }
+  }
