16ba4f3908e28783b81bbb9ace9e61daea758070
SamerSiam <ssiam05@gmail.com>
2021-12-20 21:42:12 +0200

    mod4

--- React/00-react-project/src/App.js
+++ React/00-react-project/src/App.js
@@ -1,48 +1,20 @@
 
 import React from 'react';
-import axios from 'axios';
-import Button from './Components/Button';
+import Input from './Components/Input';
 import './App.css'
 
-
-
     class App extends React.Component{
-       state={data:'',categories:[]}
-
-       async componentDidMount(){
-        const {data}=await axios.get ('https://api.chucknorris.io/jokes/categories')
-        this.setState({categories: data});
-        console.log(this.state)
-       }
        
-       LoadJokeDay= async ()=>{  
-            const {data}=await axios.get ('https://api.chucknorris.io/jokes/random')
-            console.log(data)
-            this.setState({data:data.value});
-        }
+       onInputSubmit(value){
+           console.log(value);
+       }
 
-        jokeByCategory= async (cat)=>{  
-            const {data}=await axios.get (`https://api.chucknorris.io/jokes/random?category=${cat}`)
-            this.setState({data:data.value});   
-        }
-        
-        displayButtons = () => {
-            return this.state.categories.map((button) => {
-            return <Button text={button} onHandleClick = {this.jokeByCategory} />;
-            });
-          };
        
         render(){
             return (            
             
-            <div> 
-               
-                <div className='button-container'> 
-                    <Button text='Joke of the Day'onHandleClick = {this.LoadJokeDay}/>
-                        {this.displayButtons()} 
-
-                        {this.state.data}
-                </div>
+            <div className="ui  container" style={{marginTop:'10px'}}> 
+               <Input onSubmit={this.onInputSubmit}/>
             </div>
             )
         }
--- React/00-react-project/src/Components/Button.js
+++ React/00-react-project/src/Components/Button.js
@@ -8,7 +8,7 @@ import './Button.css'
         
         onHandleClick=(event)=>{
             this.props.onHandleClick(event.target.innerText)
-                console.log(event.target.innerText)
+                // console.log(event.target.innerText)
              }
 
 
