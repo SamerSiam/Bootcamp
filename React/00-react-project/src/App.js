e4e08b6770de4c7790f947268f8fec7513fd2023
SamerSiam <ssiam05@gmail.com>
2021-12-20 09:31:51 +0200

    mod12

--- React/00-react-project/src/App.js
+++ React/00-react-project/src/App.js
@@ -1,30 +1,48 @@
+
 import React from 'react';
+import axios from 'axios';
+import Button from './Components/Button';
 import './App.css'
-import Button from './Components/Button'
-const colors = ['blue','red','yellow'];
+
+
 
     class App extends React.Component{
+       state={data:'',categories:[]}
+
+       async componentDidMount(){
+        const {data}=await axios.get ('https://api.chucknorris.io/jokes/categories')
+        this.setState({categories: data});
+        console.log(this.state)
+       }
        
+       LoadJokeDay= async ()=>{  
+            const {data}=await axios.get ('https://api.chucknorris.io/jokes/random')
+            console.log(data)
+            this.setState({data:data.value});
+        }
 
-        state={color:'', message:''};
-
-        handleClick= (childData)=> {
-            this.setState({message: childData})
-            console.log(this.state.message)
+        jokeByCategory= async (cat)=>{  
+            const {data}=await axios.get (`https://api.chucknorris.io/jokes/random?category=${cat}`)
+            this.setState({data:data.value});   
         }
         
-     
-           displayButtons = () => {
-            return colors.map((button) => {
-            return <Button color={button} onHandleClick = {this.handleClick} />;
+        displayButtons = () => {
+            return this.state.categories.map((button) => {
+            return <Button text={button} onHandleClick = {this.jokeByCategory} />;
             });
           };
-
+       
         render(){
-            return (
+            return (            
+            
             <div> 
-                {this.displayButtons()}
-                 
+               
+                <div className='button-container'> 
+                    <Button text='Joke of the Day'onHandleClick = {this.LoadJokeDay}/>
+                        {this.displayButtons()} 
+
+                        {this.state.data}
+                </div>
             </div>
             )
         }
