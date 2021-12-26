4a794ef5fa1f5bd14fe0a8606b8f3755c57e5f63
SamerSiam <ssiam05@gmail.com>
2021-12-26 22:51:54 +0200

    18.4

--- React/widgets/src/App.js
+++ React/widgets/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import Checkbox from './Components/CheckBox/CheckBox'
 
 
 const App = () => {
+const arr=["one", "two", "three", "four", "five"];
 
 
     return <div>
         
-        <h1>What's the Time</h1>
+       
+        <Checkbox checkList={arr}/>
         
     </div>
 }
